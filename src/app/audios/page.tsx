5af7a938f961e863d439d1a3090e6a10025b484a
pankajkoree <pankajkori2057@gmail.com>
2025-02-04 11:03:11 +0530

    audios

--- src/app/audios/page.tsx
+++ src/app/audios/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-100 to-purple-200 p-4">
-        <h1 className="text-3xl font-bold mb-4 text-purple-700">Audio Clip</h1>
+        <h1 className="text-3xl font-bold mb-4 text-purple-700">Audio resources for mental health wellness</h1>
         {/* <audio controls className="w-full max-w-md bg-white shadow-lg rounded-lg p-2 border border-purple-300">
         <source src="/public/audio-clip1.mp3" type="audio/mp3" />
         Your browser does not support the audio element.
