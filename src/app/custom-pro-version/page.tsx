83da9d66cd2b347215fe95cf764a87c9860ca31d
pankajkoree <pankajkori2057@gmail.com>
2025-02-04 10:26:03 +0530

    custom pro version routing

---
+++ src/app/custom-pro-version/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Routing for custom pro version</h1>
+      <h2>Will require account creation process </h2>
+    </div>
+  );
+};
+
+export default page;
