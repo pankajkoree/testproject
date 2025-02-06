1e6c3c5d6bbc3a772275e4b22dac0d19b20e3bf8
pankajkoree <pankajkori2057@gmail.com>
2025-02-06 19:37:21 +0530

    ai integration api

---
+++ src/app/api/ai-content/route.ts
@@ -0,0 +1,25 @@
+const gemini_api_key = process.env.API_KEY;
+const googleAI = new GoogleGenerativeAI(gemini_api_key);
+const geminiModel = googleAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+var question = "what is the value of pie in maths ?";
+const generate = async (question) => {
+  try {
+    const prompt = question;
+    const result = await geminiModel.generateContent(prompt);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+  } catch (error) {
+    console.log("response error", error);
+  }
+};
+
+app.post("/api/content", async (req, res) => {
+  let data = req.body.question;
+  var result = await generate(data);
+  console.log(result);
+  res.json({ result: result });
+});
